8ed2b17a8e5125724df6d1a451a47caa3c2f8af2
Randall <rcaler@calpoly.edu>
2023-03-19 23:01:19 -0700

    better gravity

--- Assets/GravityOrbit.cs
+++ Assets/GravityOrbit.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GravityOrbit : MonoBehaviour
 {
-    public GameObject planet;
+    public GameObject[] planet;
     Rigidbody2D rb;
     public float gravityForce;
     public float gravityDistance;
@@ -19,12 +19,14 @@ public class GravityOrbit : MonoBehaviour
     void Update()
     {
         //add if statement to see if planet is within range
-        float dist = Vector3.Distance(gameObject.transform.position, planet.transform.position);
-        if(dist<8) {
-          Vector3 v = planet.transform.position - transform.position;
-          rb.AddForce(v.normalized*(1.0f-dist/gravityDistance)*gravityForce);
-          lookAngle=90+Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg;
-          transform.rotation=Quaternion.Euler(0f, 0f, lookAngle);
+        foreach(GameObject g in planet) {
+            float dist = Vector3.Distance(gameObject.transform.position, g.transform.position);
+            if(dist<8) {
+            Vector3 v = g.transform.position - transform.position;
+            rb.AddForce(v.normalized*(1.0f-dist/gravityDistance)*gravityForce);
+            lookAngle=90+Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg;
+            transform.rotation=Quaternion.Euler(0f, 0f, lookAngle);
+            }
         }
     }
 }
