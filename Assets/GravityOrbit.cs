b5038cb91e70ed99f53fcd1aa92e07f9a4fade86
Randall <rcaler@calpoly.edu>
2023-03-20 12:02:06 -0700

    better gravity

--- Assets/GravityOrbit.cs
+++ Assets/GravityOrbit.cs
@@ -18,14 +18,14 @@ public class GravityOrbit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //add if statement to see if planet is within range
         foreach(GameObject g in planet) {
             float dist = Vector3.Distance(gameObject.transform.position, g.transform.position);
             if(dist<8) {
-            Vector3 v = g.transform.position - transform.position;
-            rb.AddForce(v.normalized*(1.0f-dist/gravityDistance)*gravityForce);
-            lookAngle=90+Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg;
-            transform.rotation=Quaternion.Euler(0f, 0f, lookAngle);
+              Vector3 v = g.transform.position - transform.position;
+              //rb.AddForce(v.normalized*(1.0f-dist/gravityDistance)*gravityForce);
+              rb.AddForce(v.normalized*(gravityForce/dist));
+              lookAngle=90+Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg;
+              transform.rotation=Quaternion.Euler(0f, 0f, lookAngle);
             }
         }
     }
